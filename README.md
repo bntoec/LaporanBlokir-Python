c75b5c0fdbb62e23729b0f11d39397ee2624e926
bntoec <34064495+bntoec@users.noreply.github.com>
2023-03-14 12:49:21 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Project Ini di buat untuk memudahkan dalam mengirim laporan Blokir pada Grup WA, sehingga tidak perlu meng copy paste satu persatu ke grup WA
+
+
+Cara Kerjanya
+1. Isi nomer telp pada file .env (sistem kerjanya mengirimkan ke nomer hp sendiri agar mudah di foward ke grup)
+2. Pastikan paste SPK Blokiran pada file DataBlokir.txt
+3. Pilih env python
+4. Jalankan file laporanBlokir.py
+5. Tunggu hingga chrome browser tertutup dengan sendirinya
+
+Credit
+- bntoec
+- Kalebu
---
+++ laporanBlokir.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from os import getenv
+import time
+
+if __name__ == "__main__":
+    print()
+    print("\t\t=================================================")
+    print("\t\t          Program Laporan Blokir ke WA")
+    print("\t\t               Created by bntoec      ")
+    print("\t\t=================================================")
+    print()
+
+    load_dotenv()
+
+    messenger = WhatsApp()
+
+    # Rubah pake no Telp sendiri
+    messenger.find_user(getenv("NOMER_HP_MU"))
+
+    with open('DataBlokir.txt', 'r') as file:
+        data = file.read().split('"=============================')
+        for i in range(0, len(data)):
+            messenger.send_message(data[i]) 
+            time.sleep(1)
+    
+    print()
+    print()
+    print("Laporan telah selesai di kirim, mohon untuk di cek kembali")
+    print("Jika sudah OK, silahkan foward ke grup")
+    print()
+    print("Console ini sudah dapat di tutup (CTRL+C)")
+    # ini script biar logout setelah kirim
+    # messenger.logout()
